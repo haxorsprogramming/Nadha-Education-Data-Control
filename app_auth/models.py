7735da36e0bd4606f76f4df3911a8ca59b2219f9
Haxorsprogramming <haxors@uinsu.ac.id>
2021-07-18 05:29:44 +0700

    update

---
+++ app_auth/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150)
+    kata_sandi = models.CharField(max_length=200)
+    tipe_user = models.CharField(max_length=30)
+    class Meta:
+        db_table = "tbl_user"
