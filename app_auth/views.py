ff7dbe25b8f0a27628c3dbc7c6df2b18623a912f
Haxorsprogramming <haxors@uinsu.ac.id>
2021-07-19 13:06:51 +0700

    'update'

--- app_auth/views.py
+++ app_auth/views.py
@@ -6,12 +6,12 @@ from django.http import JsonResponse
 # Create your views here.
 def home_page(request):
     context = {
-        'app_name' : 'Nadha-Education-Data-Control'
+        'app_name' : 'Nadha Education Data Control'
     }
     return render(request, 'auth/home-page.html', context)
 
 def login_page(request):
     context = {
-        'app_name' : 'Nadha-Education-Data-Control'
+        'app_name' : 'Nadha Education Data Control'
     }
     return render(request, 'auth/login-page.html', context)
---
+++ app_ppdb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppPpdbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_ppdb'
